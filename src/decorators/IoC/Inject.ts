ca18798aa0196393bf3a6ccd51c47e2ce6e59b3e
Hoan Than <hoan.than@mobytelab.com>
2020-07-28 10:45:08 +0700

    refactor

--- src/decorators/IoC/Inject.ts
+++ src/decorators/IoC/Inject.ts
@@ -1,8 +1,7 @@
 import { Containers } from "../../containers";
 import { MetadataNames } from "../interfaces";
 
-
-export const Inject: PropertyDecorator = (target: any, propertyName: string | symbol) => {
+export const Inject: () => PropertyDecorator = () => (target: any, propertyName: string | symbol) => {
     const type = Reflect.getMetadata('design:type', target, propertyName);
     const providedIn = Reflect.getMetadata(MetadataNames.ServiceProvidedIn, type);
     const serviceSymbol = Reflect.getMetadata(MetadataNames.ServiceSymbol, type);
